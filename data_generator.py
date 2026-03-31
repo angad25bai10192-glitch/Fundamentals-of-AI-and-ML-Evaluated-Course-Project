3e89c4c1a71e07a5d88e670e5d724d5dd5bcfee0
angad singh khanuja <angad.25bai10192@vitbhopal.ac.in>
2026-03-31 13:37:05 +0530

    data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -6,8 +6,6 @@ def generate_data(n=100, save_path="data/sales_data.csv"):
     np.random.seed(42)
     
     price = np.linspace(10, 100, n)
-    
-    # Non-linear demand (more realistic)
     demand = 300 - 2.5*price + 0.02*(price**2) + np.random.normal(0, 10, n)
     
     df = pd.DataFrame({
